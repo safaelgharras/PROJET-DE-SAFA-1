6c06d06c2a1c02e6077dae7c754d970e48e84901
safaelgharras <ehsafaa7@gmail.com>
2025-09-16 13:27:56 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
-let age = 20
-if (age < 18) {
-    console.log("Vous êtes mineur.");
-    } else if (age === 18) {
-        console.log("vous venez d'atteindre la majorité !");
-    } else {
-        console.log("Vous êtes majeur.");
-    }
+function addition(a, b) {
+    return a + b;
+  }
+  
+  let somme = addition(8, 5);
+  console.log("La somme est : " + somme);
