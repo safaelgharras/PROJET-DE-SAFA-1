d93d74817b405b3829a30a6fe2230ca857758e6f
safaelgharras <ehsafaa7@gmail.com>
2025-09-16 15:10:03 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-let nombre1 = 10;
-let nombre2 = 5;
-let somme = nombre1 + nombre2;
-console.log("La somme est : " + somme);
-
-
-function addition(a, b) {
-    return a + b;
-  }
-  
-  let somme = addition(8, 5);
-  console.log("La somme est : " + somme);  
-
-  
-let age = 20;
-
-if (age < 18) {
-  console.log("Vous êtes mineur.");
-} else if (age === 18) {
-  console.log("Vous venez d’atteindre la majorité !");
-} else {
-  console.log("Vous êtes majeur.");
+for (let i = 1; i <= 10; i++ ) {
+    if (i===5)continue; 
+    console.log("Nombre : " + i);
 }
